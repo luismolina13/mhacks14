9580e76b19f94e14be0ef8faa4ad60b1e1e1bd8c
Luis Molina <luismol@umich.edu>
2014-09-05 23:07:51 -0400

    Initial push with Parse support

---
+++ mhacks/mhacks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mhacks
+//
+//  Created by Luis Molina on 9/5/14.
+//  Copyright (c) 2014 Luis Molina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
